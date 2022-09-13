d15033c12cd8c08d60b1cf9047d0ffd6e98ce091
Boian Petkantchin <boian@nod-labs.com>
2022-09-12 17:22:13 -0700

    [mlir][ccl] Add chains

--- mlir/include/mlir/Dialect/CCL/IR/CCL.h
+++ mlir/include/mlir/Dialect/CCL/IR/CCL.h
@@ -12,6 +12,7 @@
 #include "mlir/IR/BuiltinTypes.h"
 #include "mlir/IR/Dialect.h"
 #include "mlir/IR/OpDefinition.h"
+#include "mlir/IR/OpImplementation.h"
 
 #include "mlir/Dialect/CCL/IR/CCLOpsEnums.h.inc"
 
